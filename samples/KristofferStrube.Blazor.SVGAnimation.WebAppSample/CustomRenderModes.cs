666feaf18b2f17bff259d32e12a28b9bc605ab10
KristofferStrube <kristoffer.strube@gmail.com>
2023-12-04 21:10:12 +0100

    Added .NET 8 Web App example.

---
+++ samples/KristofferStrube.Blazor.SVGAnimation.WebAppSample/CustomRenderModes.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace KristofferStrube.Blazor.SVGAnimation.WebAppSample;
+public static class CustomRenderModes
+{
+    public static readonly InteractiveServerRenderMode InteractiveServerRenderModeNoPreRender
+        = new InteractiveServerRenderMode(prerender: false);
+}
